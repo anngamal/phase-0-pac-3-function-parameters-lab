e6ae77d6a58365f0f6f389eb31e39c98986cc203
ann gamal <anngamal14@gmail.com>
2023-07-14 12:59:32 -0400

    completed lab

--- index.js
+++ index.js
@@ -9,3 +9,4 @@ function introduction(name) {
 function introductionWithLanguageOptional(name, language= "JavaScript"){
    return(`Hi, my name is ${name} and I am learning to program in ${language}.`)
 }
+console.log ("i")
