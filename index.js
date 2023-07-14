d604eceae5f5be91c2da8b41235bec5cd6d5b55b
ann gamal <anngamal14@gmail.com>
2023-07-14 12:40:34 -0400

    completed lab

---
+++ index.js
@@ -0,0 +1,11 @@
+function introduction(name) {
+    return(`Hi, my name is ${name}.`)
+}
+  
+  console.log(introduction("luis"))
+  function introductionWithLanguage(name, language) {
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`)
+  }
+function introductionWithLanguageOptional(name, language= "JavaScript"){
+   return(`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
